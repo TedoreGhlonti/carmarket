9a4e92661c45a5292781d18385a23cad87417079
TedoreGhlonti <tedore.ghlonti@gmail.com>
2026-08-07 17:18:05 +0400

    Enable media file serving in production

--- carmarket/urls.py
+++ carmarket/urls.py
@@ -3,7 +3,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,4 +12,8 @@ urlpatterns = [
     path('cars/', include('cars.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# media serving პირდაპირ - static() helper-ის გვერდის ავლით,
+# რომელიც production-ში (DEBUG=False) თავისით უარს ამბობს
+urlpatterns += [
+    path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
+]
